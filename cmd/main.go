df4d3988ccc7b6d8fb7b38ef66e6ced81d09f004
Denys Taradada <e_den9611@live.com>
2023-12-24 14:55:34 +0100

    update: refactor code for server shutdown and database connection closure

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"errors"
 	log "github.com/sirupsen/logrus"
 	"go_rocket_launch_sub/internal/app/server"
 	"go_rocket_launch_sub/internal/config"
 	"go_rocket_launch_sub/internal/pkg/handler"
 	"go_rocket_launch_sub/internal/pkg/repository"
 	"go_rocket_launch_sub/internal/pkg/service"
+	"gorm.io/gorm"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
@@ -21,11 +25,15 @@ func main() {
 	services := service.NewService(repositories, smtpCreds)
 	handlers := handler.NewHandler(services)
 
-	app := new(server.App)
+	srv := new(server.App)
 
 	go func() {
-		if err := app.Run(os.Getenv("PORT"), handlers.InitRoutes()); err != nil {
-			log.Fatalf("Error occurred while running http server: %s", err.Error())
+		if err := srv.Run(os.Getenv("PORT"), handlers.InitRoutes()); err != nil {
+			if errors.Is(err, http.ErrServerClosed) {
+				log.Info("HTTP server stopped")
+			} else {
+				log.Fatalf("Error occurred while running HTTP server: %s", err.Error())
+			}
 		}
 	}()
 
@@ -33,5 +41,24 @@ func main() {
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
 	<-quit
 
+	if err := srv.Shutdown(context.Background()); err != nil {
+		log.Errorf("error occured on server shutting down: %s", err.Error())
+	}
+	closeDbConnection(db)
+
 	log.Println("Server is shutting down")
+}
+
+func closeDbConnection(db *gorm.DB) {
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Printf("Error on extracting sql.DB from GORM: %s", err)
+	} else {
+		err = sqlDB.Close()
+		if err != nil {
+			log.Printf("Error on closing the database: %s", err)
+		} else {
+			log.Println("Database connection closed successfully")
+		}
+	}
 }
