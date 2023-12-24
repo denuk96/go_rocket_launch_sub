6bf571c780ebad03443b402c99c01ab8d0a5bfeb
Denys Taradada <e_den9611@live.com>
2023-12-24 14:40:27 +0100

    feat: Changed server startup code to run in a goroutine and added server shutdown logic.

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,8 @@ import (
 	"go_rocket_launch_sub/internal/pkg/repository"
 	"go_rocket_launch_sub/internal/pkg/service"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -19,8 +21,17 @@ func main() {
 	services := service.NewService(repositories, smtpCreds)
 	handlers := handler.NewHandler(services)
 
-	server := new(server.App)
-	if error := server.Run(os.Getenv("PORT"), handlers.InitRoutes()); error != nil {
-		log.Fatal("Error occured %s", error.Error())
-	}
+	app := new(server.App)
+
+	go func() {
+		if err := app.Run(os.Getenv("PORT"), handlers.InitRoutes()); err != nil {
+			log.Fatalf("Error occurred while running http server: %s", err.Error())
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	log.Println("Server is shutting down")
 }
